649854a5ca3868d8c3f70bcc5fa46e1632d0f02a
MR ERROR <156649697+MR-ERROR-707@users.noreply.github.com>
2024-06-21 09:08:04 +0600

    Update error.py

--- error.py
+++ error.py
@@ -4,4 +4,4 @@ if bit == '64bit':
  os.system("mv _req.cpython-311.so /data/data/com.termux/files/usr/lib/python3.11/_req.cpython-311.so")
  import error
 elif bit == '32bit':
- exit(" 32 bin not work 😒")
+ exit(" 32 bit not work 😒")
